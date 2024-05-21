4d3656d7a6260cbb1cf488124da000f4d8159ef2
vgsalt <144659394+vgsalt@users.noreply.github.com>
2024-05-21 10:10:42 +0200

    changes

--- code/menu.js
+++ code/menu.js
@@ -1,9 +1,9 @@
 
 const mybtn = document.getElementById('myList');
 const tre = document.getElementById('btn');
-tre.addEventListener("click", openmenu );
+tre.addEventListener("click", openmenu);
 function openmenu() {
-    if(mybtn.style.display != 'block') {
+    if (mybtn.style.display != 'block') {
         mybtn.style.display = 'block';
     } else {
         mybtn.style.display = 'none';
@@ -14,23 +14,22 @@ function openmenu() {
 // map settings
 const attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 
-var map = L.map('map1').setView([59.745164250056135, 10.164131070531106], 15);
-let marker = L.marker([59.745164250056135,10.164131070531106 ]).addTo(map)
-let tileURL =   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { }).addTo(map);
-const tiles =L.tileLayer(tileURL,{attribution})
-let place = document.getElementById("searchbar").value;
-const api_url = 'https://nominatim.openstreetmap.org/search?format=json&q=' + place; 
-console.log(place);
+var map = L.map('map1').setView([39.732, -103.228], 4);
+let tileURL = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(map);
+const tiles = L.tileLayer(tileURL, { attribution })
 
-
-async function show_me(){
-
-    let response = await fetch(api_url);
-    //let response = await fetch('https://api.openbrewerydb.org/breweries');
+async function show_me() {
+    // Get place each time the function is triggered, or you can't change the search value
+    let place = document.getElementById("searchbar").value;
+    // Get breweries for the users place
+    let response = await fetch(`https://api.openbrewerydb.org/v1/breweries/search?query=${place}&by_country=United%20States&per_page=200`);
     let data = await response.json();
     console.log(data);
     data.forEach(element => {
-        let marker = L.marker([element.lat, element.lon]).addTo(map);
-        marker.bindPopup(`<b>${element.name}</b><br>${element.lat}  ${element.lon}`).openPopup();
+        console.log(element);
+        // No lat or lon, bail
+        if (element.latitude == null || element.longitude == null) return;
+        let marker = L.marker([element.latitude, element.longitude]).addTo(map);
+        marker.bindPopup(`<b>${element.name}</b><br>${element.address_1}, ${element.city}, ${element.state_province}<br>Phone no. ${element.phone}<br><a href="${element.website_url}">Website</a>`).openPopup();
     });
 }
